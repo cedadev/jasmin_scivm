cec15cc6bee20e9947f0fa3122d6090a621d8b13
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2012-10-24 12:34:09 +0100

    Create package structure and setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='basic_package',
+      version=version,
+      description="JASMIN Scientific Analysis Build Test Suite",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='',
+      author='Stephen Pascoe',
+      author_email='Stephen.Pascoe@stfc.ac.uk',
+      url='http://proj.badc.rl.ac.uk/cedaservices/wiki/JASMIN',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup', 'examples']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'nose',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
