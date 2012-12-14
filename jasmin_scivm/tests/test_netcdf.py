d0d8fde5ce73c02eaebbe2dbcfbcfb2c31401d6d
Stephen Pascoe <Stephen.Pascoe@stfc.ac.uk>
2012-12-14 10:03:12 +0000

    Test for NetCDF Fortran support.  Refactor NetCDF test into multiple tests.

--- jasmin_scivm/tests/test_netcdf.py
+++ jasmin_scivm/tests/test_netcdf.py
@@ -5,18 +5,35 @@ Test netCDF libraries are available and properly configured
 
 import subprocess as sp
 import re
+from unittest import TestCase
 
-def test_nc_config():
+class TestNetCDF(TestCase):
     """
     Ensure the nc-config command is present and that it indicates
-    the existence of HDF and OPeNDAP support.
-
+    the existence of Fortran, HDF and OPeNDAP support.
+    
     """
-    p = sp.Popen("nc-config --all", shell=True, stdout=sp.PIPE)
+    def setUp(self):
+        p = sp.Popen("nc-config --all", shell=True, stdout=sp.PIPE)
+        self.header = p.stdout.read()
+        
 
-    header = p.stdout.read()
+    def test_nc_config(self):
+        # Ensure NetCDF 4.x distribution
+        assert re.search(r'netCDF 4\.\d+\.\d+', self.header)
 
-    assert re.search(r'netCDF 4\.\d+\.\d+', header)
-    assert re.search(r'--has-dap.*-> yes', header)
-    assert re.search(r'--has-nc4.*-> yes', header)
-    assert re.search(r'--has-hdf5.*-> yes', header)
+    def test_dap(self):
+        # Ensure DAP support
+        assert re.search(r'--has-dap.*-> yes', self.header)
+
+    def test_nc4(self):
+        # Ensure NetCDF4 format support
+        assert re.search(r'--has-nc4.*-> yes', self.header)
+        assert re.search(r'--has-hdf5.*-> yes', self.header)
+
+    def test_fortran(self):
+        # Ensure f77 and f90 support
+        assert re.search(r'--has-f77.*-> yes', self.header)
+        assert re.search(r'--has-f90.*-> yes', self.header)
+
+
