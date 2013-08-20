0f9cac7a67d7321e589bdfdecaa37eca97c9f267
Stephen pascoe <Stephen.Pascoe@stfc.ac.uk>
2013-08-20 14:28:55 +0100

    Cleaning up test_nco.py

--- jasmin_scivm/tests/test_nco.py
+++ jasmin_scivm/tests/test_nco.py
@@ -1,13 +1,5 @@
 """
-Test cdo operators
-
-
-]$ ncks jasmin_scivm/tests/data/foo.nc 
-Summary of jasmin_scivm/tests/data/foo.nc: filetype = NC_FORMAT_CLASSIC, 0 groups (max. depth = 0), 0 dimensions (0 fixed, 0 record), 0 variables (0 atomic-type, 0 non-atomic), 2 attributes (2 global, 0 group, 0 variable)
-Global attributes:
-Global attribute 0: Conventions, size = 6 NC_CHAR, value = CF-1.0
-Global attribute 1: bar, size = 1 NC_INT, value = 1
-
+Test nco operators
 
 
 """
