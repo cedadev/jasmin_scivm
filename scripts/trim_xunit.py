9d3bff4a405b64bc6ac060bf1d5dc6bd6db18bfb
Stephen pascoe <Stephen.Pascoe@stfc.ac.uk>
2013-11-17 22:16:47 +0000

    Script to clean up xunit output

---
+++ scripts/trim_xunit.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""
+Process an XUnit XML file to shorten test names thus making them easier to use
+in reporting.
+
+"""
+
+import sys
+from xml.etree import ElementTree as ET
+import hashlib
+import re
+
+SHORTEN_ATTRS = ('name', 'classname')
+MAX_ATTR_LENGTH = 60
+
+def shorten_attr(elem, attr):
+    val = elem.get(attr, '')
+
+    if len(val) > MAX_ATTR_LENGTH:
+        # Normalise whitespace
+        val = re.sub(r'\s+', ' ', val, flags=re.DOTALL | re.MULTILINE)
+
+        match = re.match(r'(.*?)\((.*)\)$', val)
+        if match:
+            name, args = match.groups()
+            md5 = hashlib.md5(args)
+            short_val = '%s#%s' % (name, md5.hexdigest()[:6])
+        else:
+            md5 = hashlib.md5(val[MAX_ATTR_LENGTH-8:])
+            short_val = '%s!%s' % (val[:MAX_ATTR_LENGTH-8], md5.hexdigest()[:6])
+        elem.set(attr, short_val)
+
+def main(argv=sys.argv):
+    xunit_xml_in, xunit_xml_out = argv[1:]
+    xunit_et = ET.parse(open(xunit_xml_in))
+    
+
+    for elem in xunit_et.findall('//testcase'):
+        for attr in SHORTEN_ATTRS:
+            shorten_attr(elem, attr)
+
+    with open(xunit_xml_out, 'w') as fh:
+        xunit_et.write(fh)
+
+    
+
+if __name__ == '__main__':
+    main()
